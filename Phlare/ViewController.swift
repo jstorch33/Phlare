3e2aed1c2dedef7d44fb972e5d2e278b4606ee69
BrianHLi <librian303@gmail.com>
2017-02-20 14:04:21 -0800

    First commit

---
+++ Phlare/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Phlare
+//
+//  Created by Brian Li on 2/13/17.
+//  Copyright © 2017 Brian Li. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    // MARK: Properties
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var goButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // initialize IBOutlets
+        username.delegate = self
+        password.delegate = self
+        
+        // facebook login
+        var loginButton = FBSDKLoginButton()
+        loginButton.center = self.view.center
+        self.view.addSubview(loginButton)
+        
+        // stylize the submit button
+        goButton.layer.opacity = 0.8
+        goButton.layer.cornerRadius = 5
+        goButton.backgroundColor = UIColor.gray
+        goButton.clipsToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    // MARK: Actions
+    @IBAction func didLogin(_ sender: Any) {
+        
+    }
+    
+
+
+}
+
