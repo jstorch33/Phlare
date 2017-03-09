1227ea6a46d20b72d487cd9a9aea444183b61cf1
dannyolaugh <danolaughlin@Daniels-MacBook-Pro-4.local>
2017-03-08 21:45:52 -0800

    Last Commit

--- Phlare/ViewController.swift
+++ Phlare/ViewController.swift
@@ -5,16 +5,13 @@
 //  Created by Brian Li on 2/13/17.
 //  Copyright © 2017 Brian Li. All rights reserved.
 //
-
 import UIKit
 import FBSDKLoginKit
 
-
 class ViewController: UIViewController, UITextFieldDelegate, FBSDKLoginButtonDelegate
 {
+    var myString = "dog"
 
-    var myString = ""
-    
     // MARK: Properties
     @IBOutlet weak var profilePicture: UIImageView!
     @IBOutlet weak var backgroundImage: UIImageView!
@@ -23,25 +20,44 @@ class ViewController: UIViewController, UITextFieldDelegate, FBSDKLoginButtonDel
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        // Do any additional setup after loading the view, typically from a nib.
         //facebook login button
+        
         let loginButton = FBSDKLoginButton()
         loginButton.alpha = 0.95
         loginButton.frame = CGRect(x: 85, y: 410, width: 200, height: 40)
-        //loginButton.center = self.view.center
+        
         view.addSubview(loginButton)
         loginButton.delegate = self
         
         // background image
         backgroundImage.image = #imageLiteral(resourceName: "background")
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start
+        {
+            (connection, result, err) in
+            if err != nil
+            {
+                print("fail:", err)
+                return
+            }
+
+            guard let data = result as? [String:Any] else { return }
+            let fbid = data["id"]
+            let username = data["name"]
+            print("Your Facebook ID is: \(fbid!)")
+            print("Your UserName is: \(username!)")
         
+            self.myString = fbid! as! String
+            print("This is the value of mystring: \(self.myString) ")
+            print(result!)
+        }
+            
         // flame image
         flameImage.image = #imageLiteral(resourceName: "burn2")
         flameImage.alpha = 0.8
-        
     }
-    
+
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!)
     {
         if error != nil
@@ -49,32 +65,8 @@ class ViewController: UIViewController, UITextFieldDelegate, FBSDKLoginButtonDel
             print("Something went wrong...\(error)")
             return
         }
+        
         print("Successfully logged in Facebook")
-        
-        
-        
-        //var result
-        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start
-        {
-            (connection, result, err) in
-            
-            if err != nil
-            {
-                print("fail:", err)
-                return
-            }
-            
-            guard let data = result as? [String:Any] else { return }
-            let fbid = data["id"]
-            let username = data["name"]
-            
-            print("Your Facebook ID is: \(fbid!)")
-            print("Your UserName is: \(username!)")
-            
-            self.myString = fbid! as! String
-            print(result!)
-        }
-        
         self.performSegue(withIdentifier: "showMap", sender: self)
     }
 
@@ -82,7 +74,7 @@ class ViewController: UIViewController, UITextFieldDelegate, FBSDKLoginButtonDel
     {
         print("Successfully logged out of Facebook")
     }
-    
+
     // MARK: UITextFieldDelegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool
     {
@@ -92,8 +84,13 @@ class ViewController: UIViewController, UITextFieldDelegate, FBSDKLoginButtonDel
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
-        let DestViewController : MapViewController = segue.destination as! MapViewController
-        DestViewController.LabelText = myString
+        if(segue.identifier == "showMap")
+        {
+            let DestViewController : MapViewController = segue.destination as! MapViewController
+            DestViewController.LabelText = self.myString
+            print("This is the value of mystring in prepareforsegue: \(self.myString) ")
+        }
     }
 }
+
 
