4f9f9a39cf9769700769c4e0c20d3780a5a97790
Jack Storch <jstorch33@gmail.com>
2017-03-11 18:20:47 -0800

    Beverley Laurel -Tame Impala

---
+++ Phlare/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  Phlare
+//
+//  Created by Jack Storch on 3/11/17.
+//  Copyright © 2017 Brian Li. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
